051e605a2717c48eb11b41d0c7f66c84e6c1be4c
Ian Fernandez <d.ian.b@live.com>
2019-02-05 14:58:47 -0200

    add some features

--- init.el
+++ init.el
@@ -15,13 +15,14 @@
  '(org-export-html-postamble nil)
  '(org-hide-leading-stars t)
  '(org-journal-dir "~/sync/orgfiles")
+ '(org-reveal-plugins (quote (classList markdown highlight zoom notes)))
  '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (htmlize ox-reveal neotree neon-mode closql weather-metno all-the-icons-gnus all-the-icons-ivy all-the-icons-dired company-tern ess-view ess-R-data-view org-pomodoro ob-sql-mode ob-clojurescript org-journal selectric-mode pretty-symbols markdown-mode pandoc-mode quelpa-use-package quelpa ob-ipython pipenv clojure-mode-extra-font-locking emms-player-mpv-jp-radios emms-info-mediainfo slack gnugo yaml-mode flymake-yaml flx nlinum-relative ac-geiser sly-repl-ansi-color sly-quicklisp ac-sly sly geiser counsel-spotify spotify multi-term haskell-emacs flycheck-clojure haskell-mode ac-cider ibuffer-tramp pretty-mode better-defaults ein py-autopep8 linum-relative vimrc-mode cyberpunk-theme auto-auto-indent color-theme try which-key use-package solarized-theme rainbow-delimiters python-mode parinfer org-link-minor-mode nyan-mode evil-smartparens evil-org evil-magit evil-cleverparens edn auto-complete))))
+    (emmet-mode org-download org-ref org-reveal org-reval rjsx-mode jsx-mode indium csv-mode org-babel-eval-in-repl scad-preview scad-mode ob-clojure-literate htmlize ox-reveal neotree neon-mode closql weather-metno all-the-icons-gnus all-the-icons-ivy all-the-icons-dired company-tern ess-view ess-R-data-view org-pomodoro ob-sql-mode ob-clojurescript org-journal selectric-mode pretty-symbols markdown-mode pandoc-mode quelpa-use-package quelpa ob-ipython pipenv clojure-mode-extra-font-locking emms-player-mpv-jp-radios emms-info-mediainfo slack gnugo yaml-mode flymake-yaml flx nlinum-relative ac-geiser sly-repl-ansi-color sly-quicklisp ac-sly sly geiser counsel-spotify spotify multi-term haskell-emacs flycheck-clojure haskell-mode ac-cider ibuffer-tramp pretty-mode better-defaults ein py-autopep8 linum-relative vimrc-mode cyberpunk-theme auto-auto-indent color-theme try which-key use-package solarized-theme rainbow-delimiters python-mode parinfer org-link-minor-mode nyan-mode evil-smartparens evil-org evil-magit evil-cleverparens edn auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "goldenrod")))))
+ )
